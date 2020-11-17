6e18d2b3f481d310e66b362641ca18ef7e126c90
m52go <mfiver@gmail.com>
2020-11-17 00:08:57 -0500

    Remove footer links

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -20,34 +20,7 @@ export class Footer extends Component<FooterProps, FooterState> {
       <nav class="container navbar navbar-expand-md navbar-light navbar-bg p-0 px-3 mt-2">
         <div className="navbar-collapse">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <span class="navbar-text">{this.props.site.version}</span>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/modlog">
-                {i18n.t('modlog')}
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link className="nav-link" to="/instances">
-                {i18n.t('instances')}
-              </Link>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href={'/docs/index.html'}>
-                {i18n.t('docs')}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href={repoUrl}>
-                {i18n.t('code')}
-              </a>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href={joinLemmyUrl}>
-                {i18n.t('join_lemmy')}
-              </a>
-            </li>
+            <li class="nav-item"></li>
           </ul>
         </div>
       </nav>
