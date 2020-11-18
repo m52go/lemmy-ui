acb9040dd448137ed582d027eee0841be9a0e6dd
m52go <mfiver@gmail.com>
2020-11-17 21:04:41 -0500

    Customize docker build script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 new_tag="$1"
 
-sudo docker build . --tag dessalines/lemmy-ui:$new_tag
-sudo docker push dessalines/lemmy-ui:$new_tag
+sudo docker build . --tag m52go/lemmy-ui:$new_tag
+sudo docker push m52go/lemmy-ui:$new_tag
 
 git tag $new_tag
-git push origin $new_tag
+git push gh $new_tag
